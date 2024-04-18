8de54bbd6c9832e869674bbe7e5a24515907ebc6
Sven van Steenis <sven.van.steenis@no.experis.com>
2024-04-18 13:47:26 +0200

    ♻️ refactor ProgressBar to be more flexible

--- src/components/Progress/ProgressBar.tsx
+++ src/components/Progress/ProgressBar.tsx
@@ -3,23 +3,45 @@ import './ProgressBar.css'
 
 export type ProgressBarProps = {
   percentage: number
+  barColor?: string
+  labelColor?: string
+  fontColor?: string
+  shadow?: boolean
+  shadowColor?: string
 }
 
-export const ProgressBar = ({ percentage, ...rest }: ProgressBarProps) => {
+export const ProgressBar = ({
+  percentage,
+  barColor,
+  labelColor,
+  fontColor,
+  shadow = true,
+  shadowColor,
+  ...rest
+}: ProgressBarProps) => {
+  const customStyles = {
+    ...(barColor && { '--bar-color': barColor }),
+    ...(labelColor && { '--label-color': labelColor }),
+    ...(fontColor && { '--font-color': fontColor }),
+    ...(shadowColor && { '--shadow-color': fontColor }),
+  } as React.CSSProperties
+
   return (
-    <div className="pie-wrapper m-5" style={{ '--percentage': percentage } as React.CSSProperties} {...rest}>
-      <span className="pie-label has-text-weight-bold">
-        {percentage}
-        <span className="label-percentage">%</span>
-      </span>
-      <div className="pie" style={percentage > 50 ? { clip: 'rect(auto, auto, auto, auto)' } : {}}>
-        <div className="left-side half-circle" style={{ transform: `rotate(${percentage * 3.6}deg)` }}></div>
-        <div
-          className="right-side half-circle"
-          style={percentage <= 50 ? { display: 'none' } : { transform: 'rotate(180deg)' }}
-        ></div>
+    <div className="radial-progress-wrapper" style={customStyles} {...rest}>
+      <div className="rp-container" style={{ '--percentage': percentage } as React.CSSProperties}>
+        <span className="rp-label has-text-weight-bold">
+          {percentage}
+          <span className="rp-label-percentage">%</span>
+        </span>
+        <div className="rp-circle" style={percentage > 50 ? { clip: 'rect(auto, auto, auto, auto)' } : {}}>
+          <div className="rp-half-circle" style={{ transform: `rotate(${percentage * 3.6}deg)` }}></div>
+          <div
+            className="rp-half-circle"
+            style={percentage <= 50 ? { display: 'none' } : { transform: 'rotate(180deg)' }}
+          ></div>
+        </div>
+        {shadow && <div className="rp-shadow"></div>}
       </div>
-      <div className="shadow"></div>
     </div>
   )
 }
