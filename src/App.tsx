b972ac640f60487ebf1608e0245e2c34983f41a2
Sven van Steenis <sven.van.steenis@no.experis.com>
2024-04-18 10:15:28 +0200

    🐛 move footer out of main container

--- src/App.tsx
+++ src/App.tsx
@@ -39,68 +39,70 @@ function App() {
   }, [])
 
   return (
-    <div className="container">
-      {resevoirData && regions && (
-        <>
-          <section className="section">
-            <div className="section is-flex is-align-items-center is-justify-content-center">
-              <ProgressBar percentage={parseFloat(convertToPercentage(resevoirData.NO.fyllingsgrad))} />
+    <>
+      <div className="container">
+        {resevoirData && regions && (
+          <>
+            <section className="section">
+              <div className="section is-flex is-align-items-center is-justify-content-center">
+                <ProgressBar percentage={parseFloat(convertToPercentage(resevoirData.NO.fyllingsgrad))} />
 
-              <div className="block">
-                <h1 className="title is-1">Fyllingsgrad</h1>
-                <h2 className="subtitle is-3">Gjennomsnitt fyllingsgrad i hele norge</h2>
+                <div className="block">
+                  <h1 className="title is-1">Fyllingsgrad</h1>
+                  <h2 className="subtitle is-3">Gjennomsnitt fyllingsgrad i hele norge</h2>
 
-                <progress
-                  className="progress is-small is-primary"
-                  value={convertToPercentage(resevoirData.NO.fyllingsgrad)}
-                  max="100"
-                >
-                  {convertToPercentage(resevoirData.NO.fyllingsgrad)}%
-                </progress>
+                  <progress
+                    className="progress is-small is-primary"
+                    value={convertToPercentage(resevoirData.NO.fyllingsgrad)}
+                    max="100"
+                  >
+                    {convertToPercentage(resevoirData.NO.fyllingsgrad)}%
+                  </progress>
+                </div>
               </div>
-            </div>
-          </section>
+            </section>
 
-          <section className="section">
-            <h2 className="title">Elspot</h2>
-            <Grid type="fixed">
-              {resevoirData.EL.map((item) => {
-                const info = regions.elspot.find((i) => i.omrnr === item.omrnr)
+            <section className="section">
+              <h2 className="title">Elspot</h2>
+              <Grid type="fixed">
+                {resevoirData.EL.map((item) => {
+                  const info = regions.elspot.find((i) => i.omrnr === item.omrnr)
 
-                return (
-                  <Grid.Cell>
-                    <StatCard
-                      key={`${item.omrType}-${item.omrnr}`}
-                      name={info?.navn_langt || ''}
-                      description={info?.beskrivelse || ''}
-                      level={item.fyllingsgrad}
-                    />
-                  </Grid.Cell>
-                )
-              })}
-            </Grid>
-          </section>
-          <section className="section">
-            <h2 className="title">Vassdrag</h2>
-            <Grid type="fixed">
-              {resevoirData.VASS.map((item) => {
-                const info = regions.vassdrag.find((i) => i.omrnr === item.omrnr)
+                  return (
+                    <Grid.Cell>
+                      <StatCard
+                        key={`${item.omrType}-${item.omrnr}`}
+                        name={info?.navn_langt || ''}
+                        description={info?.beskrivelse || ''}
+                        level={item.fyllingsgrad}
+                      />
+                    </Grid.Cell>
+                  )
+                })}
+              </Grid>
+            </section>
+            <section className="section">
+              <h2 className="title">Vassdrag</h2>
+              <Grid type="fixed">
+                {resevoirData.VASS.map((item) => {
+                  const info = regions.vassdrag.find((i) => i.omrnr === item.omrnr)
 
-                return (
-                  <Grid.Cell>
-                    <StatCard
-                      key={`${item.omrType}-${item.omrnr}`}
-                      name={info?.navn_langt || ''}
-                      description={info?.beskrivelse || ''}
-                      level={item.fyllingsgrad}
-                    />
-                  </Grid.Cell>
-                )
-              })}
-            </Grid>
-          </section>
-        </>
-      )}
+                  return (
+                    <Grid.Cell>
+                      <StatCard
+                        key={`${item.omrType}-${item.omrnr}`}
+                        name={info?.navn_langt || ''}
+                        description={info?.beskrivelse || ''}
+                        level={item.fyllingsgrad}
+                      />
+                    </Grid.Cell>
+                  )
+                })}
+              </Grid>
+            </section>
+          </>
+        )}
+      </div>
       <footer className="footer">
         <div className="content has-text-centered">
           <p>
@@ -110,7 +112,7 @@ function App() {
           {resevoirData && <p>Sist oppdadert: {resevoirData.date}</p>}
         </div>
       </footer>
-    </div>
+    </>
   )
 }
 
